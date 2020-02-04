2fc6036af7d21e84fd26f1b3e8ab17cdf02a9a37
sheapa <55968932+sheapa@users.noreply.github.com>
2020-02-03 19:59:47 -0800

    Created function for pulling stored values from local storage &populating them into placeholder text for time blocks.

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,21 @@
 $( document ).ready(function() {
-    //var aValue = storage.getItem(keyName);
-    //Built form object for handling hourly day planner input.
+
+    //Function for pulling keys & values from local storage and setting values to appropriate time block.
+    var keys = Object.keys(localStorage);
+    for (var i = 0;  i < keys.length; i++) {
+        console.log(keys[i]);
+        var selectedTextArea = $("#" + keys[i] + " #timeBlock")[0];
+        selectedTextArea.placeholder = localStorage.getItem(keys[i]);   
+    }
+    
+    // Function for setting appointment values and linking them to time block key, then saving in local storage..
     $( "form" ).submit(function( event ) { 
         var agendaTime = $(this).attr("id");
         var agendaText = $("#" + agendaTime + " :input[type=text]").val(); 
         console.log(agendaTime, agendaText);        
         localStorage.setItem(agendaTime, agendaText);
             event.preventDefault();
+
     });
 });
-
-
-// Get the value from a checked checkbox
-//$( "input[type=checkbox][name=bar]:checked" ).val(); 
-// $("label[for="+$(this).attr("id")+"]")
-//$("#" + agendaTime + " > input")
-var aValue = storage.getItem(keyName);
 
