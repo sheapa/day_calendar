03a373019df2d27bcca43d48deae9f280f60bfec
sheapa <55968932+sheapa@users.noreply.github.com>
2020-02-03 21:55:30 -0800

    Current date displayed in header using moment.

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,34 @@
 $( document ).ready(function() {
 
+    // Moment Variables
+    var m = moment()
+    var mDate = m.format("MM-DD-YYYY")
+
+    console.log(mDate);
+    var mTime = m.format("HH")
+    console.log(mTime);
+    var nowTime = "data" + mTime
+    console.log(nowTime);
+
+    //Apply moment variables
+    
+    //Display current date inside header.
+    $("#currentDay").append("<span>"+mDate+"</span>");
+
+    
+           
+
     //Function for pulling keys & values from local storage and setting values to appropriate time block.
     var keys = Object.keys(localStorage);
     for (var i = 0;  i < keys.length; i++) {
         console.log(keys[i]);
         var selectedTextArea = $("#" + keys[i] + " #timeBlock")[0];
-        selectedTextArea.placeholder = localStorage.getItem(keys[i]);   
+        selectedTextArea.placeholder = localStorage.getItem(keys[i]); 
+        
+        //if ((mTime > 17) || (mTime < 8)) {
+    
+         //   $("input").css("Background-Color: red")
+       // }
     }
     
     // Function for setting appointment values and linking them to time block key, then saving in local storage..
